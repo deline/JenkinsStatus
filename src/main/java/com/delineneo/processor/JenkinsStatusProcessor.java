b60e9f5a8dc29fdfc1741deacabb910a3fbb4af7
Deline Neo <deline@delinemac.(none)>
2012-03-04 00:30:40 +1100

    add SerialCommunicator class for comms, implement processor tests for build success and failure scenarios

--- src/main/java/com/delineneo/processor/JenkinsStatusProcessor.java
+++ src/main/java/com/delineneo/processor/JenkinsStatusProcessor.java
@@ -1,9 +1,14 @@
 package com.delineneo.processor;
 
+import com.delineneo.communication.SerialCommunicator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static org.apache.commons.lang.StringUtils.contains;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,18 +20,40 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class JenkinsStatusProcessor {
     private static final String JENKINS_URL = "http://localhost:8080/job/JenkinsStatus/api/json" ;
+    private static final String SUCCESS_BUILD_COLOR = "blue";
+    public static final char BUILD_FAIL = '1';
+    public static final char BUILD_SUCCESS = '0';
+
     private RestTemplate restTemplate;
+    private SerialCommunicator serialCommunicator;
 
     @Scheduled(fixedDelay=5000)
     public void process() {
 
         String jsonString = restTemplate.getForObject(JENKINS_URL, String.class);
 
-        System.out.println(">>>>>>>>> in the process method");
+        boolean buildSuccess = isBuildSuccessful(jsonString);
+        try {
+            serialCommunicator.send(buildSuccess ? BUILD_SUCCESS : BUILD_FAIL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isBuildSuccessful(String jsonString) {
+        if (contains(jsonString, SUCCESS_BUILD_COLOR)) {
+            return true;
+        }
+        return false;
     }
 
     @Autowired
     public void setRestTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    @Autowired
+    public void setSerialCommunicator(SerialCommunicator serialCommunicator) {
+        this.serialCommunicator = serialCommunicator;
     }
 }
--- src/test/java/com/delineneo/processor/JenkinsStatusProcessorTest.java
+++ src/test/java/com/delineneo/processor/JenkinsStatusProcessorTest.java
@@ -1,10 +1,12 @@
 package com.delineneo.processor;
 
+import com.delineneo.communication.SerialCommunicator;
 import org.easymock.EasyMockSupport;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.fail;
@@ -28,28 +30,46 @@ public class JenkinsStatusProcessorTest extends EasyMockSupport {
             "\"url\":\"http://localhost:8080/job/JenkinsStatus/4/\"},\"lastFailedBuild\":{\"number\":4,\"url\":\"http://localhost:8080/job/JenkinsStatus/4/\"},\"lastStableBuild\":null," +
             "\"lastSuccessfulBuild\":null,\"lastUnstableBuild\":null,\"lastUnsuccessfulBuild\":{\"number\":4,\"url\":\"http://localhost:8080/job/JenkinsStatus/4/\"}," +
             "\"nextBuildNumber\":5,\"property\":[{}],\"queueItem\":null,\"concurrentBuild\":false,\"downstreamProjects\":[],\"scm\":{},\"upstreamProjects\":[]}";
+    
+    private static final String PASSING_BUILD_JSON = "{\"actions\":[{},{}],\"description\":\"\",\"displayName\":\"JenkinsStatus\",\"displayNameOrNull\":null," +
+            "\"name\":\"JenkinsStatus\",\"url\":\"http://localhost:8080/job/JenkinsStatus/\",\"buildable\":true,\"builds\":[{\"number\":7,\"url\":\"http://localhost:8080/job/JenkinsStatus/7/\"}," +
+            "{\"number\":6,\"url\":\"http://localhost:8080/job/JenkinsStatus/6/\"},{\"number\":5,\"url\":\"http://localhost:8080/job/JenkinsStatus/5/\"}," +
+            "{\"number\":4,\"url\":\"http://localhost:8080/job/JenkinsStatus/4/\"},{\"number\":3,\"url\":\"http://localhost:8080/job/JenkinsStatus/3/\"}]," +
+            "\"color\":\"blue\",\"firstBuild\":{\"number\":3,\"url\":\"http://localhost:8080/job/JenkinsStatus/3/\"},\"healthReport\":[{\"description\":\"Build stability: 4 out of the last 5 builds failed.\"," +
+            "\"iconUrl\":\"health-00to19.png\",\"score\":20}],\"inQueue\":false,\"keepDependencies\":false,\"lastBuild\":{\"number\":7,\"url\":\"http://localhost:8080/job/JenkinsStatus/7/\"}," +
+            "\"lastCompletedBuild\":{\"number\":7,\"url\":\"http://localhost:8080/job/JenkinsStatus/7/\"},\"lastFailedBuild\":{\"number\":6,\"url\":\"http://localhost:8080/job/JenkinsStatus/6/\"}," +
+            "\"lastStableBuild\":{\"number\":7,\"url\":\"http://localhost:8080/job/JenkinsStatus/7/\"},\"lastSuccessfulBuild\":{\"number\":7,\"url\":\"http://localhost:8080/job/JenkinsStatus/7/\"}," +
+            "\"lastUnstableBuild\":null,\"lastUnsuccessfulBuild\":{\"number\":6,\"url\":\"http://localhost:8080/job/JenkinsStatus/6/\"},\"nextBuildNumber\":8,\"property\":[{}],\"queueItem\":null," +
+            "\"concurrentBuild\":false,\"downstreamProjects\":[],\"scm\":{},\"upstreamProjects\":[]}";
 
     private JenkinsStatusProcessor jenkinsStatusProcessor;
     private RestTemplate restTemplate = createMock("restTemplate", RestTemplate.class);
+    private SerialCommunicator serialCommunicator = createMock("serialCommunicator", SerialCommunicator.class);
 
     @Before
     public void setup() {
         jenkinsStatusProcessor = new JenkinsStatusProcessor();
         jenkinsStatusProcessor.setRestTemplate(restTemplate);
+        jenkinsStatusProcessor.setSerialCommunicator(serialCommunicator);
     }
 
     @Test
-    public void failingBuildShouldSendMessageToTurnLedOn() {
-
+    public void failingBuildShouldSendMessageToTurnLedOn() throws Exception {
         expect(restTemplate.getForObject(JENKINS_URL, String.class)).andReturn(FAILING_BUILD_JSON);
-
+        serialCommunicator.send('1');
+        
         replayAll();
         jenkinsStatusProcessor.process();
         verifyAll();
     }
 
-    @Ignore
-    public void passingBuildShouldSendMessageToTurnLedOff() {
-        fail("Test not yet implemented");
+    @Test
+    public void passingBuildShouldSendMessageToTurnLedOff() throws Exception {
+        expect(restTemplate.getForObject(JENKINS_URL, String.class)).andReturn(PASSING_BUILD_JSON);
+        serialCommunicator.send('0');
+
+        replayAll();
+        jenkinsStatusProcessor.process();
+        verifyAll();
     }
 }
